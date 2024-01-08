5d5b6e009e2181e1f03b5b893a553e3e139014c7
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-08 12:00:07 +0900

    🍰 :: Called Spring Initializr

---
+++ src/main/java/me/seula/ync/SpringYncApplication.java
@@ -0,0 +1,13 @@
+package me.seula.ync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringYncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringYncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seula/ync/SpringYncApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seula.ync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringYncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
