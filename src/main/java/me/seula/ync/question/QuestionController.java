e334f0ad706a6d000092d3808ba08ee9a917656d
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-10 10:19:46 +0900

    🍔 :: QuestionController 주입 빈 변경

--- src/main/java/me/seula/ync/question/QuestionController.java
+++ src/main/java/me/seula/ync/question/QuestionController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -12,11 +11,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<QuestionEntity> questionList = questionRepository.findAll();
+        List<QuestionEntity> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
