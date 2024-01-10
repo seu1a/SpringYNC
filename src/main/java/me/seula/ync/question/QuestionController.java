8bbd12ae71ac8dc7aea5187424b78a28a9051000
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-10 09:32:22 +0900

    📬 :: Model에 questionList 추가

--- src/main/java/me/seula/ync/question/QuestionController.java
+++ src/main/java/me/seula/ync/question/QuestionController.java
@@ -1,16 +1,24 @@
 package me.seula.ync.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+    public String list(Model model) {
+        List<QuestionEntity> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 
 }
