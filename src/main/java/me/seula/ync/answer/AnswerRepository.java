03f6e9a13325d71cb77e19b570cefd10f393bbdb
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-10 11:43:45 +0900

    🌭 :: Bootstrap 적용

--- src/main/java/me/seula/ync/answer/AnswerRepository.java
+++ src/main/java/me/seula/ync/answer/AnswerRepository.java
@@ -1,6 +1,5 @@
 package me.seula.ync.answer;
 
-import me.seula.ync.answer.AnswerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Integer> {
