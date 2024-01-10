859713269fa8d090ab3a1fbebc40262cfe1fe772
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-10 10:41:33 +0900

    🍣 :: question_detail HTML 추가

--- src/main/java/me/seula/ync/question/QuestionService.java
+++ src/main/java/me/seula/ync/question/QuestionService.java
@@ -1,10 +1,12 @@
 package me.seula.ync.question;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -15,6 +17,15 @@ public class QuestionService {
 
     public List<QuestionEntity> getList() {
         return questionRepository.findAll();
+    }
+
+    public QuestionEntity getQuestion(Integer id) {
+        Optional<QuestionEntity> question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new EntityNotFoundException("Question not found");
+        }
     }
 
 }
