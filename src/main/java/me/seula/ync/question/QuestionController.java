f81315cb962e4eed2761a53326a6755b6827ddb4
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-10 11:07:52 +0900

    🍿 :: RequestMapping 처리

--- src/main/java/me/seula/ync/question/QuestionController.java
+++ src/main/java/me/seula/ync/question/QuestionController.java
@@ -5,23 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<QuestionEntity> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         QuestionEntity question = questionService.getQuestion(id);
         model.addAttribute("question", question);
