c3555e9f7607caf6bda18e4783bc20d491b5deec
rheeseula <rheeseula@dgsw.hs.kr>
2024-01-11 10:18:24 +0900

    🍎 :: AnswerController 수정

--- src/main/java/me/seula/ync/answer/AnswerController.java
+++ src/main/java/me/seula/ync/answer/AnswerController.java
@@ -4,11 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.seula.ync.question.QuestionEntity;
 import me.seula.ync.question.QuestionService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/answer")
 public class AnswerController {
